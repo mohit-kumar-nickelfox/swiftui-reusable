287ccc5acad9889f3f7aea05914d48d6f232a7a4
Mohit Singh <117419663+mohit-kumar-nickelfox@users.noreply.github.com>
2023-06-29 11:17:52 +0530

    chore(initial-commit): add basic swiftui project

---
+++ swiftui-reusable/swiftui_reusableApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_reusableApp.swift
+//  swiftui-reusable
+//
+//  Created by Mohit Kumar Singh on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_reusableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
